ce96c7831012ea09ca3716898f55a76f2e5231d5
Stefan <sml@cs.man.ac.uk>
2012-12-02 06:08:13 +0000

    stuffs

--- EntryManagementSystem/src/ems/gui/UserManagementFrame.java
+++ EntryManagementSystem/src/ems/gui/UserManagementFrame.java
@@ -53,8 +53,15 @@ public class UserManagementFrame extends javax.swing.JFrame {
       jLabel6 = new javax.swing.JLabel();
       jTextField3 = new javax.swing.JTextField();
       jLabel7 = new javax.swing.JLabel();
-      jPanel2 = new javax.swing.JPanel();
+      jButton4 = new javax.swing.JButton();
       jPanel3 = new javax.swing.JPanel();
+      jScrollPane2 = new javax.swing.JScrollPane();
+      jTextPane1 = new javax.swing.JTextPane();
+      jButton5 = new javax.swing.JButton();
+      jTextField4 = new javax.swing.JTextField();
+      jButton6 = new javax.swing.JButton();
+      jButton7 = new javax.swing.JButton();
+      jPanel2 = new javax.swing.JPanel();
       jLabel1 = new javax.swing.JLabel();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -103,7 +110,7 @@ public class UserManagementFrame extends javax.swing.JFrame {
                   .addGroup(jPanel4Layout.createSequentialGroup()
                      .addGap(34, 34, 34)
                      .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane1)
                         .addGroup(jPanel4Layout.createSequentialGroup()
                            .addComponent(jButton1)
                            .addGap(18, 18, 18)
@@ -124,7 +131,7 @@ public class UserManagementFrame extends javax.swing.JFrame {
                .addComponent(jButton1)
                .addComponent(jButton2)
                .addComponent(jButton3))
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
             .addComponent(jLabel5)
             .addContainerGap())
       );
@@ -166,13 +173,15 @@ public class UserManagementFrame extends javax.swing.JFrame {
          .addGap(0, 250, Short.MAX_VALUE)
       );
 
-      jTabbedPane2.addTab("tab3", jPanel7);
+      jTabbedPane2.addTab("Picture", jPanel7);
 
       jLabel6.setText("Date of Birth");
 
       jTextField3.setText("jTextField3");
 
       jLabel7.setText("dd/mm/yy");
+
+      jButton4.setText("Submit");
 
       javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
       jPanel1.setLayout(jPanel1Layout);
@@ -181,10 +190,6 @@ public class UserManagementFrame extends javax.swing.JFrame {
          .addGroup(jPanel1Layout.createSequentialGroup()
             .addGap(39, 39, 39)
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-               .addGroup(jPanel1Layout.createSequentialGroup()
-                  .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                  .addGap(104, 104, 104)
-                  .addComponent(jTabbedPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 381, javax.swing.GroupLayout.PREFERRED_SIZE))
                .addGroup(jPanel1Layout.createSequentialGroup()
                   .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                      .addComponent(jLabel2)
@@ -196,7 +201,13 @@ public class UserManagementFrame extends javax.swing.JFrame {
                      .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                      .addComponent(jTextField2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                   .addGap(18, 18, 18)
-                  .addComponent(jLabel7)))
+                  .addComponent(jLabel7))
+               .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                  .addComponent(jButton4)
+                  .addGroup(jPanel1Layout.createSequentialGroup()
+                     .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                     .addGap(104, 104, 104)
+                     .addComponent(jTabbedPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 381, javax.swing.GroupLayout.PREFERRED_SIZE))))
             .addContainerGap(66, Short.MAX_VALUE))
       );
       jPanel1Layout.setVerticalGroup(
@@ -215,14 +226,67 @@ public class UserManagementFrame extends javax.swing.JFrame {
                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                .addComponent(jLabel6)
                .addComponent(jLabel7))
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 75, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-               .addComponent(jTabbedPane2, javax.swing.GroupLayout.Alignment.TRAILING)
-               .addComponent(jPanel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGap(33, 33, 33))
+            .addGap(38, 38, 38)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+               .addComponent(jTabbedPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(29, 29, 29)
+            .addComponent(jButton4)
+            .addContainerGap(18, Short.MAX_VALUE))
       );
 
       jTabbedPane1.addTab("Add User", jPanel1);
+
+      jScrollPane2.setViewportView(jTextPane1);
+
+      jButton5.setText("Load All");
+      jButton5.addActionListener(new java.awt.event.ActionListener()
+      {
+         public void actionPerformed(java.awt.event.ActionEvent evt)
+         {
+            jButton5ActionPerformed(evt);
+         }
+      });
+
+      jTextField4.setText("jTextField4");
+
+      jButton6.setText("Search");
+
+      jButton7.setText("Select User");
+
+      javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+      jPanel3.setLayout(jPanel3Layout);
+      jPanel3Layout.setHorizontalGroup(
+         jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(jPanel3Layout.createSequentialGroup()
+            .addGap(41, 41, 41)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 781, Short.MAX_VALUE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+               .addComponent(jTextField4)
+               .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+               .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+               .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGap(51, 51, 51))
+      );
+      jPanel3Layout.setVerticalGroup(
+         jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(jPanel3Layout.createSequentialGroup()
+            .addGap(52, 52, 52)
+            .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+               .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+               .addGroup(jPanel3Layout.createSequentialGroup()
+                  .addComponent(jButton5)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                  .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                  .addComponent(jButton6)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                  .addComponent(jButton7)))
+            .addContainerGap(257, Short.MAX_VALUE))
+      );
+
+      jTabbedPane1.addTab("Manage User", jPanel3);
 
       javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
       jPanel2.setLayout(jPanel2Layout);
@@ -236,19 +300,6 @@ public class UserManagementFrame extends javax.swing.JFrame {
       );
 
       jTabbedPane1.addTab("Remove User", jPanel2);
-
-      javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-      jPanel3.setLayout(jPanel3Layout);
-      jPanel3Layout.setHorizontalGroup(
-         jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGap(0, 979, Short.MAX_VALUE)
-      );
-      jPanel3Layout.setVerticalGroup(
-         jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGap(0, 506, Short.MAX_VALUE)
-      );
-
-      jTabbedPane1.addTab("tab3", jPanel3);
 
       jLabel1.setFont(new java.awt.Font("DejaVu Sans", 0, 24)); // NOI18N
       jLabel1.setText("User Management");
@@ -277,6 +328,11 @@ public class UserManagementFrame extends javax.swing.JFrame {
       pack();
    }// </editor-fold>//GEN-END:initComponents
 
+   private void jButton5ActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButton5ActionPerformed
+   {//GEN-HEADEREND:event_jButton5ActionPerformed
+      // TODO add your handling code here:
+   }//GEN-LAST:event_jButton5ActionPerformed
+
     /**
     * @param args the command line arguments
     */
@@ -292,6 +348,10 @@ public class UserManagementFrame extends javax.swing.JFrame {
    private javax.swing.JButton jButton1;
    private javax.swing.JButton jButton2;
    private javax.swing.JButton jButton3;
+   private javax.swing.JButton jButton4;
+   private javax.swing.JButton jButton5;
+   private javax.swing.JButton jButton6;
+   private javax.swing.JButton jButton7;
    private javax.swing.JLabel jLabel1;
    private javax.swing.JLabel jLabel2;
    private javax.swing.JLabel jLabel3;
@@ -308,11 +368,14 @@ public class UserManagementFrame extends javax.swing.JFrame {
    private javax.swing.JPanel jPanel6;
    private javax.swing.JPanel jPanel7;
    private javax.swing.JScrollPane jScrollPane1;
+   private javax.swing.JScrollPane jScrollPane2;
    private javax.swing.JTabbedPane jTabbedPane1;
    private javax.swing.JTabbedPane jTabbedPane2;
    private javax.swing.JTextField jTextField1;
    private javax.swing.JTextField jTextField2;
    private javax.swing.JTextField jTextField3;
+   private javax.swing.JTextField jTextField4;
+   private javax.swing.JTextPane jTextPane1;
    // End of variables declaration//GEN-END:variables
 
 }
